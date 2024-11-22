cca954bedaaeb61595e1d0083e568b7848f18b9f
amarijano <tonimarijanovic@gmail.com>
2024-11-22 15:39:35 +0100

    init commit + husky

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <form>
+      <input
+        id="dinosaur_name"
+        name="dinosaur_name"
+        className="dinosaur_classname"
+      />
+    </form>
+  );
+}
+export default App;
